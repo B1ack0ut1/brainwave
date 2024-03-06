c8fc6229c806b0cc4518699739a8fcfe92076b7c
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2024-03-06 23:20:33 +0800

    Worked on Services component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import Section from "./Section";
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
